d81ac2549b7251c67094509df18b4d4b88830f3c
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-25 13:31:41 +0100

    added animation to project card

--- app/javascript/components/moreInfoAnimation.js
+++ app/javascript/components/moreInfoAnimation.js
@@ -4,16 +4,41 @@ const card = document.querySelector('.plan-your-night')
 const heading = document.querySelector('.planyournight-heading')
 const description = document.querySelector('.hover-content')
 
+const fadeIn = {
+  easing: "ease",
+  opacity: [
+    0,
+    1
+  ]
+};
+
+const fadeOut = {
+  easing: "ease",
+  opacity: [
+    1,
+    0
+  ]
+};
+
+const timing = {
+  duration: 500,
+  fill: "forwards",
+  iterations: 1,
+  delay: 0,
+  endDelay: 0
+};
+
 const pynHover = () => {
-  card.addEventListener('mouseover', (event) => {
-    // heading.classList.add('hidden');
-    move('.planyournight-heading').set('display', 'none').end();
-    description.classList.remove('hidden');
-  });
-  card.addEventListener('mouseout', (event) => {
-    // description.classList.add('hidden');
-    move('.hover-content').set('display', 'none').end();
-    heading.classList.remove('hidden');
+  card.addEventListener('click', (event) => {
+    if (description.classList.contains('hidden')) {
+      heading.classList.add('hidden');
+      description.classList.remove('hidden');
+      description.animate(fadeIn, timing);
+    } else {
+      heading.classList.remove('hidden');
+      description.classList.add('hidden');
+      heading.animate(fadeIn, timing);
+    }
   });
 };
 
